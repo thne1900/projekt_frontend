ba2dec7ec2162511ceb75abd3131d650e25210eb
Therese Nevander <thne1900@student.miun.se>
2025-05-13 23:16:30 +0200

    Lagt till ett färgtema via en knapp. Utformat colorScheme.js och _colorscheme för att kunna välja ett ljust tema.

---
+++ src/js/colorScheme.js
@@ -0,0 +1,11 @@
+/*To change the color scheme to a lighter theme*/
+
+"use strict";
+
+let changeColor=document.getElementById("colorScheme");
+
+changeColor.addEventListener("click", ()=>{
+document.body.classList.toggle("light-theme");
+
+});
+
